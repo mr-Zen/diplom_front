91935868d00d6d2ef8bda2891f0151c508b4803b
Артем Комяк <homaone@yandex.ru>
2019-11-30 22:11:02 +0300

    process analytics js

--- src/analytics.js
+++ src/analytics.js
@@ -1,14 +1,10 @@
-import './pages/analytics.css';
-import { localKeyWords, sumTitleKeyWords, analitikMonth } from './js-modules/helpers';
-
-
-localKeyWords()
-
-sumTitleKeyWords()
-analitikMonth()
-
-// const sevenDays = Math.round(new Date().getTime()/1000-7 * 86400);
-// const dateString = new Date(sevenDays*1000);
-// const date = new Date(dateString).getDate();
-// const fullDate = `${date+1}`;
-
+import "./pages/analytics.css";
+import {
+  localKeyWords,
+  sumTitleKeyWords,
+  analitikMonth
+} from "./js-modules/helpers";
+localKeyWords();
+sumTitleKeyWords();
+analitikMonth();
+// Диаграмма в процессе
