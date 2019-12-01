0c30cf2202a3693d518441fd78094225b375c1b7
Артем Комяк <homaone@yandex.ru>
2019-12-01 17:51:01 +0300

    implemented Analytics graph

--- src/js-modules/helpers.js
+++ src/js-modules/helpers.js
@@ -1,39 +1,60 @@
 const localData = JSON.parse(localStorage.getItem("res"));
+const dateAnalytics = document.querySelectorAll(".analitik__chart-date");
+const graf = document.querySelectorAll(".analitik__chart-percent");
 
-//Вставка поскового запроса из localStorage
-let entries={}
+//Вставка данных для аналитики из localStorage
 export function sumTitleKeyWords() {
+  let entries = {};
   let sumTitle = 0;
-  let sumDescription = 0;
   localData.articles.forEach(item => {
-    sumTitle =
-      sumTitle +
-      item.title
-        .toLowerCase()
-        .split(localStorage.getItem("keywords").toLowerCase()).length -
-      1;
-      
-      sumDescription = sumDescription +
-      item.description
-      .toLowerCase()
-      .split(localStorage.getItem("keywords").toLowerCase()).length -
-    1;
-    //Общее количество вхождений запроса в title и description
-    let sum = sumTitle + sumDescription;
-  
-//     let publishDate = item.publishedAt.split('T')[0];
-//     if (publishDate in entries) {
-//         entries[publishDate]+= sum;
-//       }else{
-//           entries[publishDate] = sum
-//       }
-//       let sorted = [];
-// for (let key in entries) {
-//   sorted[sorted.length] = key;
-// }
+    let sum = 0;
+    if (item.title) {
+      sumTitle =
+        sumTitle +
+        item.title
+          .toLowerCase()
+          .split(localStorage.getItem("keywords").toLowerCase()).length -
+        1;
+      sum =
+        sum +
+        item.title
+          .toLowerCase()
+          .split(localStorage.getItem("keywords").toLowerCase()).length -
+        1;
+    }
+    if (item.description) {
+      sum =
+        sum +
+        item.description
+          .toLowerCase()
+          .split(localStorage.getItem("keywords").toLowerCase()).length -
+        1;
+    }
+    
+    let publishDate = item.publishedAt.split("T")[0];
+    if (publishDate in entries) {
+      entries[publishDate] += sum;
+    } else {
+      entries[publishDate] = sum;
+    }
 
     document.querySelector("#newsTitle").textContent = sumTitle;
   });
+  let sorted = [];
+  for (let key in entries) {
+    sorted[sorted.length] = key;
+  }
+  sorted.sort();
+  
+
+  for (let i = 0; i < 7; i++) {
+    
+    
+    dateAnalytics[i].textContent = sorted[i];
+    graf[i].textContent = entries[sorted[i]];
+    graf[i].style = "width: " + entries[sorted[i]] + "%";
+    }
+  
 }
 
 export function localKeyWords() {
@@ -49,11 +70,25 @@ export function localKeyWords() {
 }
 
 function getMonth(month) {
-    let months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь',  'декабрь'];
-  
-    return months[month.getMonth()];
-  }
+  let months = [
+    "январь",
+    "февраль",
+    "март",
+    "апрель",
+    "май",
+    "июнь",
+    "июль",
+    "август",
+    "сентябрь",
+    "октябрь",
+    "ноябрь",
+    "декабрь"
+  ];
+
+  return months[month.getMonth()];
+}
 //Дата в шапке графика
-export function analitikMonth(){
-    document.querySelector(".analitik__chart-column").textContent = 'Дата' + ' ' + '(' + getMonth(new Date) + ')'
+export function analitikMonth() {
+  document.querySelector(".analitik__chart-column").textContent =
+    "Дата" + " " + "(" + getMonth(new Date()) + ")";
 }
